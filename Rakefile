86171341937f79da74d2e3ca855d01b1f825756c
Dan Buch <daniel.buch@gmail.com>
2015-02-09 21:26:56 -0500

    Boop

---
+++ Rakefile
@@ -0,0 +1,4 @@
+task :default do
+  require './lib/chirp'
+  Chirp.new.run!
+end
---
+++ lib/chirp.rb
@@ -0,0 +1,20 @@
+class Chirp
+  def run!
+    children = []
+    Dir.glob(scripts) do |script|
+      if File.executable?(script)
+        $stdout.puts "---> #{script.inspect}"
+        children << fork { %x{#{script}} }
+      end
+    end
+    children.map { |pid| Process.waitpid(pid) }
+  end
+
+  def scripts
+    @scripts ||= File.expand_path("#{scripts_dir}/*")
+  end
+
+  def scripts_dir
+    @scripts_dir ||= ENV.fetch('CHIRP_SCRIPTS', File.expand_path('../../scripts', __FILE__))
+  end
+end
