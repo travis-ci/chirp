fc08ae915cc51e0d616d0a6b60dba09282cfc34a
Dan Buch <daniel.buch@gmail.com>
2015-02-10 11:02:27 -0500

    Add forever dots

--- lib/chirp.rb
+++ lib/chirp.rb
@@ -1,13 +1,30 @@
 class Chirp
+  def initialize
+    $stdout.sync = true
+    $stderr.sync = true
+  end
+
   def run!
-    children = []
+    children = {}
+    start_dots
+
     Dir.glob(scripts) do |script|
       if File.executable?(script)
         $stdout.puts "---> #{script.inspect}"
-        children << fork { %x{#{script} &>/dev/null} }
+        pid = fork { system script }
+        children[pid] = File.basename(script)
       end
     end
-    children.map { |pid| Process.waitpid(pid) }
+
+    loop do
+      break if children.empty?
+
+      children.clone.map do |pid, basename|
+        children.delete(pid) if Process.waitpid(pid, Process::WNOHANG)
+      end
+
+      sleep 0.2
+    end
   end
 
   def scripts
@@ -16,5 +33,14 @@ class Chirp
 
   def scripts_dir
     @scripts_dir ||= ENV.fetch('CHIRP_SCRIPTS', File.expand_path('../../scripts', __FILE__))
+  end
+
+  def start_dots
+    Thread.start do
+      loop do
+        $stderr.write '.'
+        sleep 0.5
+      end
+    end
   end
 end
