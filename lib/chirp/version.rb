b05d0a1856ea377614433709d51489788d2a166b
Dan Buch <daniel.buch@gmail.com>
2016-10-12 15:44:26 -0400

    Bundler and rubocop updates

--- lib/chirp/version.rb
+++ lib/chirp/version.rb
@@ -1,4 +1,5 @@
 # vim:fileencoding=utf-8
+# frozen_string_literal: true
 module Chirp
   VERSION = '0.1.0'
 end
--- spec/chirp_spec.rb
+++ spec/chirp_spec.rb
@@ -1,4 +1,5 @@
 # vim:fileencoding=utf-8
+# frozen_string_literal: true
 describe Chirp do
   it 'has a Runner' do
     expect(Chirp::Runner).to_not be_nil
